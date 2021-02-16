de8f7b6bfeb45c6f473ef27e76a6715a5fcbee74
Jaeyoon <sbjeong222@gmail.com>
2021-02-16 15:37:00 +0900

    test

---
+++ thrid-month-test/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Counter from "./Counter";
+import Say from "./Say";
+
+const App = () => {
+  return <Say />;
+};
+
+export default App;
---
+++ thrid-month-test/src/Say.js
@@ -0,0 +1,17 @@
+import React, { Component, useState } from "react";
+
+const Say = () => {
+  const [message, setMessage] = useState("");
+  const onClickEnter = () => setMessage("hi");
+  const onClickLeave = () => setMessage("bye");
+
+  return (
+    <div>
+      <button onClick={onClickEnter}>입장</button>
+      <button onClick={onClickLeave}>퇴장</button>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default Say;
