720a336f40e864392de17a332ae89b27e4e22f85
Jaeyoon <sbjeong222@gmail.com>
2022-01-04 00:00:55 +0900

    Add: App.css 작성

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ function App() {
     const name = "리액트";
 
     return (
-        <div
-            style={{
-                backgroundColor: "black",
-                color: "blue",
-                fontSize: "50px",
-            }}
-        >
-            {name}
-        </div>
+        <>
+            <div className="react">{name}</div>
+            <input />
+        </>
     );
 }
 
