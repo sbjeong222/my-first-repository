6c12249109cd7a460b4f05987fb1dffdb2ba88fe
Jaeyoon <sbjeong222@gmail.com>
2022-01-03 23:18:15 +0900

    Add: And 연산자 사용

--- src/App.js
+++ src/App.js
@@ -2,16 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    const name = "리액트";
-    return (
-        <div>
-            {name === "리액트" ? (
-                <h1>리액트 입니다.</h1>
-            ) : (
-                <h2>리액트가 아닙니다.</h2>
-            )}
-        </div>
-    );
+    const name = "뤼액트";
+    return <div>{name === "리액트" && <h1>리액트 입니다!</h1>}</div>;
 }
 
 export default App;
