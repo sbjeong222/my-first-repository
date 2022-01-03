c6b5487eeb2ca21a82872b7cd986acf327b2b089
Jaeyoon <sbjeong222@gmail.com>
2022-01-03 23:33:31 +0900

    Add: 인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    const name = "뤼액트";
-    return <div>{name === "리액트" && <h1>리액트 입니다!</h1>}</div>;
+    const name = "리액트";
+
+    return (
+        <div
+            style={{
+                backgroundColor: "black",
+                color: "blue",
+                fontSize: "50px",
+            }}
+        >
+            {name}
+        </div>
+    );
 }
 
 export default App;
