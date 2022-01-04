4e7b41692144c2174d353633b4105364e29e91b3
Jaeyoon <sbjeong222@gmail.com>
2022-01-04 19:41:54 +0900

    Add: Component 종류 확인

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-    const name = "리액트";
+//함수형 컴포넌트.
+// function App() {
+//     const name = "리액트";
 
-    return (
-        <>
-            <div className="react">{name}</div>
-            <input />
-        </>
-    );
+//     return <div className="react">{name}</div>;
+// }
+
+// export default App;
+
+
+//클래스형 컴포넌트.
+class App extends Component {
+    render() {
+        const name = "react";
+        return <div className="react">{name}</div>;
+    }
 }
 
 export default App;
